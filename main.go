4ff59cb63631eeda72b850b67a23a7cfc65c442a
Wessel Kuipers <WesselKuipers@users.noreply.github.com>
2022-06-02 23:53:27 +0200

    Fix day parameter in music command
    Closes #18

--- main.go
+++ main.go
@@ -603,7 +603,18 @@ var (
 			monthName := retrievedMonth.StartTime.Format("Jan 2006")
 			day := now.Day()
 			if len(i.Data.Options) > 1 {
-				day = int(i.Data.Options[1].IntValue())
+				newDay := int(i.Data.Options[1].IntValue())
+				if newDay >= currentMonthStart.Day() && newDay <= currentMonthEnd.Day() {
+					day = newDay
+				} else {
+					s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+						Type: discordgo.InteractionResponseChannelMessageWithSource,
+						Data: &discordgo.InteractionApplicationCommandResponseData{
+							Content: "The given day is invalid.",
+						},
+					})
+					return
+				}
 			}
 
 			var response strings.Builder
